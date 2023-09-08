0c8e9058b9c64609204f855b55fc84768cbb8370
Kris.Pajak <krzysztof.pajak@relexsolutions.com>
2023-09-08 15:35:37 +0100

    Dot

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-    // small change for a push
+    // small change for a push.
     return <div>
             <h1>Hi There</h1>
             <hr/>
